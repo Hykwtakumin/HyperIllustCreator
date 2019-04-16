d58953a64376ced3edb096de8b85f7885efef495
Takumi Hayakawa <Hykwtakumin@users.noreply.github.com>
2019-04-16 13:05:56 +0900

    ハイパーリンク埋め込み機能の実装
    選択したパスに対してSVGAnchorElementを追加できるようになった

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,104 @@
 import "./style.css";
 
 const svgCanvas = document.getElementById("svgCanvas");
+svgCanvas.setAttribute("width", (window.innerWidth * 0.8).toString());
+svgCanvas.setAttribute("height", (window.innerHeight * 0.6).toString());
+svgCanvas.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+svgCanvas.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
 
+const colorPicker = document.getElementById("color");
 const undoButton = document.getElementById("undo");
 const redoButton = document.getElementById("redo");
 const clearButton = document.getElementById("clear");
+const addLinkButton = document.getElementById("addLink");
+const dlButton = document.getElementById("download");
+
+const dialog = document.getElementById("dialog") as HTMLDialogElement;
+const modalCancel = document.getElementById("modalCancel");
+const modalConfirm = document.getElementById("modalConfirm");
 
 svgCanvas.addEventListener("pointerdown", handlePointerDown);
 svgCanvas.addEventListener("pointermove", handlePointerMove);
 svgCanvas.addEventListener("pointerup", handlePointerUp);
 
+colorPicker.addEventListener("change", handleColorChange);
 undoButton.addEventListener("click", handleUndo);
 redoButton.addEventListener("click", handleRedo);
 clearButton.addEventListener("click", handleClear);
+addLinkButton.addEventListener("click", handleAddLink);
+dlButton.addEventListener("click", handelDlButton);
 document.addEventListener("keypress", handleKeyPress);
 
+modalCancel.addEventListener("click", handleModalCancel);
+modalConfirm.addEventListener("click", handleModalConfirm);
+
 let isDragging: boolean = false;
+let isSelecting: boolean = false;
 let movetoX: number = 0;
 let movetoY: number = 0;
-let strokeColor: string = "#666666";
+let strokeColor: string = "#585858";
 let linetoStr: string = "";
 let drawType: string = "";
 let lastPath: SVGElement;
 let oldPath: SVGElement;
+let selectedPath: SVGElement;
 
 let linetoX = [], //描画点の横座標の初期化
   linetoY = [], //描画点の縦座標の初期化
   cntMoveto: number = 0; //描画点のカウンターを初期化
 
 function handlePointerDown(event: PointerEvent) {
-  console.log("down");
-  isDragging = true;
+  if (!isSelecting) {
+    isDragging = true;
 
-  movetoX = event.pageX - svgCanvas.clientLeft;
-  movetoY = event.pageY - svgCanvas.clientTop;
+    movetoX = event.pageX - svgCanvas.clientLeft;
+    movetoY = event.pageY - svgCanvas.clientTop;
 
-  const pathElm: SVGPathElement = document.createElementNS(
+    const pathElm: SVGPathElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+
+    pathElm.setAttribute("d", "");
+    pathElm.setAttribute("fill", "none");
+    pathElm.setAttribute("stroke", strokeColor);
+    pathElm.setAttribute("stroke-width", (6 * event.pressure).toString());
+    lastPath = pathElm;
+    svgCanvas.appendChild(pathElm);
+    (linetoX = []), //描画点の横座標の初期化
+      (linetoY = []), //描画点の縦座標の初期化
+      (cntMoveto = 0); //描画点のカウンターを初期化
+    linetoStr = "M " + movetoX + " " + movetoY + " "; //d要素でpathの開始点を設定
+  } else {
+    console.dir(event.srcElement);
+    selectedPath = event.srcElement as SVGElement;
+    //selectedPath.classList.add("selectedPath");
+    dialog.showModal();
+    console.dir(dialog.returnValue);
+    isSelecting = false;
+  }
+}
+
+function handleModalCancel() {
+  dialog.close();
+  isSelecting = false;
+}
+
+function handleModalConfirm() {
+  const link = document.getElementById("pathLink") as HTMLInputElement;
+  const appendLink = link.value;
+  console.log(`link : ${appendLink}`);
+  const linkElm: SVGAElement = document.createElementNS(
     "http://www.w3.org/2000/svg",
-    "path"
+    "a"
   );
-
-  pathElm.setAttribute("d", "");
-  pathElm.setAttribute("fill", "none");
-  pathElm.setAttribute("stroke", strokeColor);
-  pathElm.setAttribute("stroke-width", (5 * event.pressure).toString());
-  lastPath = pathElm;
-  svgCanvas.appendChild(pathElm);
-  (linetoX = []), //描画点の横座標の初期化
-    (linetoY = []), //描画点の縦座標の初期化
-    (cntMoveto = 0); //描画点のカウンターを初期化
-  linetoStr = "M " + movetoX + " " + movetoY + " "; //d要素でpathの開始点を設定
+  linkElm.setAttribute("xlink:href", appendLink);
+  linkElm.setAttribute("target", "_blank");
+  selectedPath.appendChild(linkElm);
+  selectedPath.parentNode.insertBefore(linkElm, selectedPath);
+  const copyPath = svgCanvas.removeChild(selectedPath);
+  linkElm.appendChild(copyPath);
+  dialog.close();
 }
 
 function handlePointerMove(event: PointerEvent) {
@@ -69,7 +118,9 @@ function handlePointerUp(event: PointerEvent) {
   isDragging = false;
 }
 
-function handleColorChange(event) {}
+function handleColorChange(event) {
+  strokeColor = event.target.value;
+}
 
 function handleUndo() {
   if (svgCanvas.lastChild) {
@@ -88,6 +139,27 @@ function handleClear() {
   while (svgCanvas.firstChild) {
     svgCanvas.removeChild(svgCanvas.firstChild);
   }
+}
+
+function handleAddLink() {
+  isDragging = false;
+  isSelecting = true;
+}
+
+function handelDlButton() {
+  const fileName = "hyperillust.svg";
+
+  const blobObject: Blob = new Blob(
+    [new XMLSerializer().serializeToString(svgCanvas)],
+    { type: "image/svg+xml;charset=utf-8" }
+  );
+  const dlUrl = window.URL.createObjectURL(blobObject);
+  const dlLink = document.createElement("a");
+  document.body.appendChild(dlLink);
+  dlLink.setAttribute("href", dlUrl);
+  dlLink.setAttribute("target", "_blank");
+  dlLink.setAttribute("download", fileName);
+  dlLink.click();
 }
 
 function handleKeyPress(event: KeyboardEvent) {
